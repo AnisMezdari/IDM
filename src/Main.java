8e5e50e49a1ec4e1a8c6010418d7fc382e6e80e6
Anismezdari <anis.mezdari@gmail.com>
2022-09-15 14:48:38 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		ArrayList<String> test = new ArrayList<String>();
+		
+		test.add("test");
+		test.add("onestla");
+		test.add("juste chute enfaite");
+		
+		for(String item : test){
+			System.out.print(item + " ");
+		}
+	}
+	
+	
+}
